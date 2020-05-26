7c4f05eee75f23de5241a1f1906c357e956e683a
Pavel <65237025+Pashadark@users.noreply.github.com>
2020-05-27 00:54:18 +0300

    Update main.py 27.05

--- main.py
+++ main.py
@@ -18,67 +18,114 @@ from string import Template
 import requests
 import datetime
 
-
 # Токены
 bot = telebot.TeleBot(config.token)
 now = datetime.datetime.now()
-# Классы
-# предварительно создаем переменную psw ( рандомное число )
+
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
+# Классы
 user_dict = {}
 
 
-# Классы
 class User:
     def __init__(self, category):
         self.category = category
 
-        keys = ['fullname', 'phone', 'address', 'message', 'end', 'serial']
+        keys = ['fullname', 'phone', 'address', 'message', 'head', 'beard']
 
         for key in keys:
             self.key = None
 
+
+### Функция проверки авторизации
+def autor(chatid):
+    strid = str(chatid)
+    for item in config.users:
+        if item == strid:
+            return True
+    return False
+
+
 @bot.message_handler(commands=['help'])
 def help_command(message):
     bot.send_message(message.chat.id,
-        '1) Если бот завис и не работает нажмите /start.\n' +
-        '2) Если у вас есть предложение напишите мне.\n' +
-        '3) Список команд нажмите /commands \n' +
-        '4) Время и дата на сервере /time \n',
-        reply_markup=keyboard.keyboardhelp)
+                      '1) Если бот завис и не работает нажмите /start\n' +
+                      '2) Команды для администратора /admin\n' +
+                      '3) Список команд нажмите /commands\n' +
+                      '4) Время и дата на сервере /time \n' +
+                      '5) Если у вас есть предложение напишите мне⤵',
+                      reply_markup=keyboard.keyboardhelp)
+
 
 @bot.message_handler(commands=['time'])
 def time_command(message):
-    bot.send_message(message.chat.id, 'Время и дата на сервере\n'+now.strftime("%d-%m-%Y %H:%M"))
+    bot.send_message(message.chat.id, 'Время и дата на сервере\n' + now.strftime("%d-%m-%Y %H:%M"))
+
+bot.message_handler (commands=['list'])
+def list_command(message):
+    bot.send_message(message.chat.id, 'Список администраторов')
+
+
+
+@bot.message_handler (commands=['admin'])
+def start_message(message):
+    if autor(message.chat.id):
+        cid = message.chat.id
+        message_text = message.text
+        user_id = message.from_user.id
+        user_name = message.from_user.first_name
+        # mention = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+        bot.send_message (message.chat.id, 'Привет администратор', reply_markup=keyboard.admin)
+        bot.send_message (message.chat.id, 'Вот пару команд для тебя', reply_markup=keyboard.admin)
+    else:
+        bot.send_message (message.chat.id,
+                          'Вы не являетесь администратором для выполнения этой команды' + str (message.chat.id))
+
 
 @bot.message_handler (commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет, я пока в тестовом режиме работаю. \nБудут проблемы нажми /help', reply_markup=keyboard.keyboard1)
-    bot.send_chat_action (message.chat.id, 'typing')
-    time.sleep(1)
-    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAINkl7AH2HI5BFg7fTaSd5cvBR67-hOAAKMCAACeVziCYUNIF_-rII7GQQ')
+    bot.send_message (message.chat.id, 'Привет, я пока в тестовом режиме работаю. \nБудут проблемы нажми /help',
+                      reply_markup=keyboard.keyboard1)
+
+
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text == '✂Барбер':
-        msg = bot.send_message(message.chat.id, 'Выберите мастера', reply_markup=keyboard.barber)
-        bot.send_chat_action(message.chat.id, 'typing')
+        bot.send_chat_action (message.chat.id, 'typing')
         time.sleep(1)
+        msg = bot.send_message(message.chat.id, 'Выберите мастера', reply_markup=keyboard.barber)
         bot.register_next_step_handler(msg, process_category_step)
+
 def process_category_step(message):
     try:
         chat_id = message.chat.id
         user_dict[chat_id] = User(message.text)
-        bot.send_chat_action(message.chat.id, 'typing')
+        bot.send_chat_action (message.chat.id, 'typing')
         time.sleep(1)
-        msg = bot.send_message(message.chat.id, 'Выберите услугу оказанную мастером', reply_markup=keyboard.service)
+        msg = bot.send_message(message.chat.id, 'Че по голове?', reply_markup=keyboard.service1)
+        bot.register_next_step_handler(msg, process_head_step)
+
+    except Exception as e:
+        bot.reply_to(message, 'Ой что-то не то!!')
+
+
+def process_head_step(message):
+    try:
+        chat_id = message.chat.id
+        user = user_dict[chat_id]
+        user.head = message.text
+        bot.send_chat_action (message.chat.id, 'typing')
+        time.sleep(1)
+        msg = bot.send_message(message.chat.id, 'Че по бороде?', reply_markup=keyboard.service2)
         bot.register_next_step_handler(msg, process_end_step)
 
     except Exception as e:
-        bot.reply_to(message, 'ooops!!')
+        bot.reply_to(message, 'Ой что-то не то!!')
+
 
 def process_end_step(message):
     try:
@@ -87,27 +134,31 @@ def process_end_step(message):
         user.end = message.text
 
         # Говорим о создании заявки
+        bot.send_chat_action(message.chat.id, 'typing')
+        time.sleep(1)
         bot.send_message(chat_id, getRegData(user, 'Мы все учли, отправляем...', message.from_user.first_name),
                           parse_mode="Markdown", reply_markup=keyboard.keyboard1)
-        bot.send_message(message.chat.id, 'Готово', reply_markup=keyboard.keyboard1)
+        time.sleep (2)
+        bot.send_message (message.chat.id, 'Готово', reply_markup=keyboard.keyboard1)
         # Отправляем в группу
-        bot.send_chat_action(message.chat.id, 'typing')
         bot.send_message(config.chat_id, getRegData(user, 'Оповещение от', bot.get_me ().username),
                           parse_mode="Markdown")
+        bot.send_chat_action(message.chat.id, 'typing')
 
     except Exception as e:
-        bot.reply_to(message, 'ooops!!')
+        bot.reply_to(message, 'Ой что-то не то!!')
 
 
 # Формирует вид заявки регистрации
 def getRegData(user, title, name):
     t = Template(
-        '$title *$name* \n\n 🧔Барбер: *$category* \n ✂Услуга: *$end* \n ⌚Дата: *$data*')
+        '$title *$name* \n\n 🧔Барбер: *$category* \n\n ✂Голова: *$head* \n ✂Борода: *$end* \n ⌚Дата: *$data*')
 
     return t.substitute({
         'title': title,
         'name': name,
         'category': user.category,
+        'head' : user.head,
         'end': user.end,
         'data': now.strftime("%d-%m-%Y %H:%M")
     })
@@ -120,19 +171,19 @@ def getRegData(user, title, name):
     # WARNING It will work only if enable_save_next_step_handlers was called!
     bot.load_next_step_handlers ()
 
-@bot.callback_query_handler(func=lambda c:True)
+@bot.callback_query_handler (func=lambda c: True)
 def inline(c):
-  if c.data == 'NumberOne':
-    bot.send_message(c.message.chat.id, 'Это кнопка 1')
-  if c.data == 'NumberTwo':
-    bot.send_message(c.message.chat.id, 'Это кнопка 2')
-  if c.data == 'NumberTree':
-    key = types.InlineKeyboardMarkup()
-    but_1 = types.InlineKeyboardButton(text="NumberOne", callback_data="NumberOne")
-    but_2 = types.InlineKeyboardButton(text="NumberTwo", callback_data="NumberTwo")
-    but_3 = types.InlineKeyboardButton(text="NumberTree", callback_data="NumberTree")
-    key.add(but_1, but_2, but_3)
-    bot.send_message(c.message.chat.id, 'Это кнопка 3', reply_markup=key)
+    if c.data == 'NumberOne':
+        bot.send_message (c.message.chat.id, 'Это кнопка 1')
+    if c.data == 'NumberTwo':
+        bot.send_message (c.message.chat.id, 'Это кнопка 2')
+    if c.data == 'NumberTree':
+        key = types.InlineKeyboardMarkup ()
+        but_1 = types.InlineKeyboardButton (text="NumberOne", callback_data="NumberOne")
+        but_2 = types.InlineKeyboardButton (text="NumberTwo", callback_data="NumberTwo")
+        but_3 = types.InlineKeyboardButton (text="NumberTree", callback_data="NumberTree")
+        key.add(but_1, but_2, but_3)
+        bot.send_message(c.message.chat.id, 'Это кнопка 3', reply_markup=key)
 
 
 # Ребут после ошибки
@@ -141,17 +192,16 @@ while True:
         bot.polling(none_stop=True)
     except:
         print('Ошибка! Пробую востановить сервер...')
-        time.sleep (1)
+        time.sleep(1)
         print('Загрузка... █ 15%')
-        time.sleep (1)
+        time.sleep(1)
         print('Загрузка... ███ 30%')
-        time.sleep (1)
+        time.sleep(1)
         print('Загрузка... ██████ 45%')
         time.sleep (1)
         print('Загрузка... █████████ 60%')
         time.sleep (1)
         print('Загрузка... ████████████ 100%')
-        time.sleep (4)
+        time.sleep(4)
         logging.error('error: {}'.format(sys.exc_info()[0]))
         time.sleep(4)
-
